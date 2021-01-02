8af017572778ced520732ec924f3b623364b73ff
Gene Pan <pangene@gmail.com>
2021-01-02 03:17:10 -0800

    Got the version buttons, changing variable, changing label done.

--- gui-blathers.py
+++ gui-blathers.py
@@ -3,7 +3,7 @@ from PIL import ImageTk, Image
 import blathers
 
 # Chalkboard, Nadeem, Gill Sans are all similar to AC font
-FONT = '', 
+FONT = 'Chalkboard'
 BACKGROUND_IMAGE = 'images/background.png'
 # RGB for textbox background: 254, 250, 230
 TEXTBOX_COLOR = '#FEFAE6'
@@ -11,73 +11,89 @@ TEXTBOX_COLOR = '#FEFAE6'
 FONT_COLOR = '#8A846E'
 BLATHERS_IMAGE = 'images/Blathers_NH.png'
 
-def startup():
+VERSIONS = blathers.Blathers.versions
+
+def create_window():
     '''Sets up the gui window with sizing, background, textboxes, etc.'''
-    global text_label
-    global window
-    global background_image
+    global text_label, version, version_label
 
     window = tk.Tk()
     window.geometry('1280x720')
-    window.columnconfigure(0, minsize=1280)
-    window.rowconfigure([0, 1, 2, 3], minsize=180)
     window.resizable(0, 0)
-    window.option_add('*Font', 'Chalkboard 26')
+    window.option_add('*Font', FONT + ' 26')
 
     background_image = ImageTk.PhotoImage(Image.open(BACKGROUND_IMAGE))
     background_label = tk.Label(window, image=background_image)
+    background_label.image = background_image
     background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    version_frame = tk.Frame(master=window, width=350, height=115, bg=TEXTBOX_COLOR)
+    version_frame.place(x=130, y=105)
+    version_frame.columnconfigure([0, 1, 2], minsize=105)
+    version_frame.rowconfigure([0, 1], minsize=50)
+    row, column = 0, 0
+    for version in VERSIONS:
+        if column == 3:
+            column = 0
+            row += 1
+        version = beautify_text(version)
+        version_button = tk.Button(master=version_frame,
+                text=version,
+                font=FONT + ' 23',
+                bg=TEXTBOX_COLOR,
+                fg=FONT_COLOR,
+                command=(lambda version: lambda: change_version(version))(version)
+            )
+        version_button.grid(row=row, column=column)
+        column += 1
+
+    version_label = tk.Label(master=window,
+            text='Current: ' + beautify_text(version),
+            bg=TEXTBOX_COLOR,
+            fg=FONT_COLOR,
+            font=FONT + ' 22'
+        )
+    print(version_label)
+    version_label.place(x=220, y=65)
 
     textbox_frame = tk.Frame(master=window,
             width=700,
             height=150,
             bg=TEXTBOX_COLOR
         )
-    textbox_frame.grid(row=2, column=0, pady=80)
-    textbox_frame.grid_propagate(False)
+    textbox_frame.place(x=280, y=510)
     text_label = tk.Label(textbox_frame,
             text='',
             anchor='w',
             bg=TEXTBOX_COLOR,
             fg=FONT_COLOR,
-            wraplength=680,
+            wraplength=675,
             justify='left',
         )
-    display_text("Welcome to the Blathers talkbot GUI! Press enter to proceed.")
-    window.bind('<Return>', lambda x: version_select)
+
+    display_text("Welcome to the Blathers talkbot GUI!")
     text_label.grid(sticky='w')
+    window.mainloop()
 
 
 def display_text(string):
+    '''Displays text in the dialogue textbox.'''
     text_label['text'] = string
 
 
-def create_text_menu(options):
-    '''Creates a text menu based off given list of options.'''
-    menu_frame = tk.Frame(master=window,
-        width=700,
-        height=200,
-        bg=TEXTBOX_COLOR,
-        highlightbackground=FONT_COLOR,
-        highlightthickness=1)
-    menu_frame.grid(row=1)
-    menu_frame.grid_propagate(False)
-    for index, option in enumerate(options):
-        option_button = tk.Button(master=menu_frame, text=option, anchor='w')
-        option_button.pack(pady=5, padx=5)
+def beautify_text(string):
+    '''Returns beautified form of Python/personal naming-convention string.'''
+    return string.replace('_', ' ').replace('-', ' ').title()
 
-def version_select():
-    display_text('Which version of Blathers would you like to speak with?')
-    versions = []
-    for version_num, version in enumerate(blathers.Blathers.versions):
-        versions.append(str(version_num + 1) + '. ' + version)
-    return versions
 
+def change_version(new_version):
+    '''Changes the version of Blathers.'''
+    global version
+    version = new_version
+    version_label['text'] = 'Current: ' + beautify_text(new_version)
 
 def main():
-    startup()
-    create_text_menu(version_select())
-    window.mainloop()
+    create_window()
 
 if __name__ == '__main__':
     main()
