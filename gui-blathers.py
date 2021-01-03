ece4e2b42853146799d653e6463bad290aa6eead
Gene Pan <pangene@gmail.com>
2021-01-03 01:31:54 -0800

    Finished up gui and getting description. Made description scrollable if too long. Updated readme. Added requirements.txt.

--- gui-blathers.py
+++ gui-blathers.py
@@ -17,7 +17,10 @@ CREATURES = ['bug', 'fish', 'deep-sea_creature']
 
 def display_text(string):
     '''Displays text in the dialogue textbox.'''
-    text_label['text'] = string
+    textbox_text['state'] = tk.NORMAL
+    textbox_text.delete(1.0, tk.END)
+    textbox_text.insert(tk.END, string)
+
 
 
 def beautify_text(string):
@@ -30,16 +33,30 @@ def change_version(new_version):
     global version
     version = new_version
     version_label['text'] = 'Current: ' + beautify_text(new_version)
-    update_creature_list(version, type_creature, connection)
+    update_type_buttons(version)
+    update_creature_list()
 
 def change_type(new_type):
     '''Changes the type of creature being requested.'''
     global type_creature
     type_creature = new_type
     type_current_label['text'] = 'Current: ' + beautify_text(new_type)
-    update_creature_list(version, type_creature, connection)
+    update_creature_list()
 
-def update_creature_list(version, type_creature, connection):
+
+def update_type_buttons(version):
+    '''Changes which buttons are present based on game version.'''
+    if version not in ['new_leaf', 'new_horizons']:
+        creature_button_identities['deep-sea_creature']['state'] = tk.DISABLED
+        creature_button_identities['fg'] = 'grey'
+        if type_creature == 'deep-sea_creature':
+            change_type('fish')
+    else:
+        creature_button_identities['deep-sea_creature']['state'] = tk.NORMAL
+        creature_button_identities['fg'] = FONT_COLOR
+
+
+def update_creature_list():
     '''Updates the creatures in the creature listbox.'''
     dialogues = blathers.get_version_creature_SQL_table(version,
         type_creature, connection)
@@ -47,6 +64,17 @@ def update_creature_list(version, type_creature, connection):
     for creature in dialogues:
         creature_listbox.insert(tk.END, creature)
 
+
+def get_creature_description():
+    dialogues = blathers.get_version_creature_SQL_table(version,
+        type_creature, connection)
+    choice = creature_listbox.get(tk.ANCHOR)
+    return dialogues[choice]
+
+
+def display_description():
+    description = get_creature_description()
+    display_text(description)
 
 
 # SQL stuff
@@ -58,7 +86,8 @@ window = tk.Tk()
 window.title('Blathers Talkbot')
 window.geometry('1280x720')
 window.resizable(0, 0)
-window.option_add('*Font', FONT + ' 26')
+window.option_add('*Font', FONT + ' 23')
+window.bind('<Return>', lambda x: display_description())
 
 # Set background image
 background_image = ImageTk.PhotoImage(Image.open(BACKGROUND_IMAGE))
@@ -106,6 +135,7 @@ type_current_label = tk.Label(master=type_frame,
         font=FONT + ' 23'
     )
 type_current_label.pack(pady=10)
+creature_button_identities = {}
 for creature in CREATURES:
     creature_button = tk.Button(master=type_frame,
             text=beautify_text(creature),
@@ -115,15 +145,17 @@ for creature in CREATURES:
             command=(lambda type_creature:
                         lambda: change_type(type_creature))(creature)
         )
+    creature_button_identities[creature] = creature_button
     creature_button.pack(side=tk.LEFT, padx=10)
-
+update_type_buttons(version)
 
 # Create creature selection
 creature_frame = tk.Frame(master=window, width=155, height=280, bg='blue')
 creature_frame.place(x=630, y=110)
 creature_scrollbar = tk.Scrollbar(creature_frame, orient=tk.VERTICAL)
 creature_listbox = tk.Listbox(master=creature_frame,
-        width=12,
+        bd = 0,
+        width=20,
         height=12,
         bg=TEXTBOX_COLOR,
         fg=FONT_COLOR,
@@ -132,7 +164,7 @@ creature_listbox = tk.Listbox(master=creature_frame,
     )
 creature_scrollbar.config(command=creature_listbox.yview)
 creature_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-update_creature_list(version, type_creature, connection)
+update_creature_list()
 creature_listbox.pack()
 
 
@@ -141,17 +173,22 @@ textbox_frame = tk.Frame(master=window,
         height=150,
         bg=TEXTBOX_COLOR
     )
-textbox_frame.place(x=280, y=510)
-text_label = tk.Label(textbox_frame,
-        text='',
-        anchor='w',
+textbox_frame.place(x=280, y=505)
+textbox_scrollbar = tk.Scrollbar(textbox_frame, orient=tk.VERTICAL)
+textbox_text = tk.Text(master=textbox_frame,
+        state=tk.DISABLED,
         bg=TEXTBOX_COLOR,
         fg=FONT_COLOR,
-        wraplength=675,
-        justify='left',
+        width=49,
+        height=5,
+        wrap=tk.WORD,
+        yscrollcommand=textbox_scrollbar.set,
+        highlightthickness=0
     )
+textbox_scrollbar.config(command=textbox_text.yview)
+textbox_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+textbox_text.pack()
 
-display_text("Welcome to the Blathers talkbot GUI!")
-text_label.grid(sticky='w')
+display_text("Welcome to the Blathers talkbot GUI! After making your selection, press enter to read the description!")
 window.mainloop()
 
